9789ee2ca01a4f39018c2a922436a767f6040c30
Kyium <kiu4537@yahoo.co.uk>
2025-05-11 12:13:54 +0100

    Added power limiter script

---
+++ cpu_power_limit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 120W limit for long and short term.
+echo 120000000 > /sys/class/powercap/intel-rapl:0/constraint_0_power
+echo 120000000 > /sys/class/powercap/intel-rapl:0/constraint_1_power_limit_uw
